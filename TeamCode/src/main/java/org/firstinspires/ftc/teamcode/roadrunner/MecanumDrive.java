4d2ed6fe0f5543268d925f4e10218665b7b5d439
ewyzzia <giikae127@gmail.com>
2025-01-22 07:55:30 -0800

    Tune mecanum drive parameters

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/MecanumDrive.java
@@ -65,7 +65,7 @@ public final class MecanumDrive {
 
         // drive model parameters
         public double inPerTick = 93.875/31870.0;
-        public double lateralInPerTick = inPerTick; // measured at the exact same thing
+        public double lateralInPerTick = inPerTick; // measured the exact same thing
         public double trackWidthTicks = 3213.936896545979;
 
         // feedforward parameters (in tick units)
@@ -79,13 +79,13 @@ public final class MecanumDrive {
         public double maxProfileAccel = 50;
 
         // turn profile parameters (in radians)
-        public double maxAngVel = Math.PI; // shared with path
+        public double maxAngVel = Math.PI/2; // shared with path
         public double maxAngAccel = Math.PI;
 
         // path controller gains
-        public double axialGain = 5.0;
-        public double lateralGain = 5.0;
-        public double headingGain = 10.0; // shared with turn
+        public double axialGain = 3.0;
+        public double lateralGain = 3.0;
+        public double headingGain = 7.0; // shared with turn
 
         public double axialVelGain = 0.0;
         public double lateralVelGain = 0.0;
